186f2374fac62c74c10f0fa76964b1e3825db9bf
salmayaser <salmayasser161@gmail.com>
2022-08-03 16:22:34 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from './models/post.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'service-workers';
+  public posts: Post[] = [];
+
+  constructor(public http: HttpClient) {
+    this.getPosts().subscribe((res) => {
+      this.posts = res;
+    });
+  }
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
+  }
 }
