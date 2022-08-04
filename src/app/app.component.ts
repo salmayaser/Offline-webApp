65e01dbc7b693817165f2e2d920dd1e9b340a644
salmayaser <salmayasser161@gmail.com>
2022-08-04 11:11:40 +0200

    Fixing some issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 import { Observable } from 'rxjs';
 import { Post } from './models/post.model';
 
@@ -12,13 +13,13 @@ export class AppComponent {
   title = 'service-workers';
   public posts: Post[] = [];
 
-  constructor(public http: HttpClient) {
+  constructor(public http: HttpClient, private updates: SwUpdate) {
     this.getPosts().subscribe((res) => {
       this.posts = res;
     });
   }
 
   getPosts(): Observable<Post[]> {
-    return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
+    return this.http.get<Post[]>('http://jsonplaceholder.typicode.com/posts');
   }
 }
